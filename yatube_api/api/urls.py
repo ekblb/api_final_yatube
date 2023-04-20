683900b3d19ca0d556a2f188a1992ffabc7b4b82
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-04-20 12:55:02 +0300

    regular expression u

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,11 +6,11 @@ from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 app_name = 'api'
 
 v1_router = DefaultRouter()
-v1_router.register(r'posts', PostViewSet)
-v1_router.register(r'groups', GroupViewSet)
+v1_router.register('posts', PostViewSet)
+v1_router.register('groups', GroupViewSet)
 v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
-v1_router.register(r'follow', FollowViewSet,
+v1_router.register('follow', FollowViewSet,
                    basename='follow')
 
 
